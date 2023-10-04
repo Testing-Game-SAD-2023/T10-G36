71fc90f444cedeba4808c2a6188e56f0bd2bfccc
Simone Argentino <93777986+Simoneargentino@users.noreply.github.com>
2023-10-04 19:06:48 +0200

    Delete app directory

--- app/src/main/java/com/sad/t5t1/project/services/iMatchService.java
+++
@@ -1,9 +0,0 @@
-package com.sad.t5t1.project.services;
-
-import com.sad.t5t1.project.model.Match_Result;
-
-public interface iMatchService {
-
-    Match_Result create(Match_Result match);
-    
-}
